550078bdfc01b1a7b6eaf2e3a4006d0dc46503fb
yuvaganesh2007 <yuvaganesh2007@gmail.com>
2025-12-29 05:13:23 -0600

    Add LSB Histogram test

--- steganalysis/imageanalysis.py
+++ steganalysis/imageanalysis.py
@@ -1,8 +1,21 @@
 import numpy as np
 from PIL import Image
 
-def LSBHistogram:
+def LSBHistogram(input_image):
+    count1=0
+    count0=0
     print("LSB Histogram text is selected")
-
-def ChiSquare:
+    img=Image.open(input_image).convert("RGB")
+    pixel_array=np.asarray(img)
+    pixel_arr=pixel_array.flatten()
+    print(len(pixel_arr))
+    for i in pixel_arr:
+        if i%2==0:
+            count0+=1
+        else:
+            count1+=1
+    print(abs(count1-count0)/(count0+count1))
+def ChiSquare(input_image):
     print("Chi Square test is selected")
+    img=Image.open(input_image).convert("RGB")
+    pixel_array=np.asarray(img)
