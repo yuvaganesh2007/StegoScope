aff07784789fa947d4d3937b148cca82f251559f
yuvaganesh2007 <yuvaganesh2007@gmail.com>
2026-01-06 12:24:55 -0600

    Add final changes

--- sample2.py
+++
@@ -1,7 +0,0 @@
-with open ("/home/yuvaganesh/StegoScope/new.txt","r") as file:
-    for line in f:
-        l=line.strip()
-        n=int(l)
-        n+=1
-        print(n)
-
--- sample3.py
+++
@@ -1,5 +0,0 @@
-import os
-p="/home/yuvaganesh/StegoScope/tmpdir"
-for roots,dirs,files in os.walk(p):
-    for filename in files:
-        print(filename)
